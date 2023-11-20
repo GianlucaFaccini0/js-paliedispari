5b826de2c601bc3d7e497d39a83b3130d97bf129
GianlucaFaccini0 <gianluca.faccini0@gmail.com>
2023-11-20 16:41:17 +0100

    Completing first exercise, adding JS script

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-/* Questo è il primo dei due esercizi richiesti - La parola sarà palindroma oppure no? */
+// Questo è il primo dei due esercizi richiesti - La parola sarà palindroma oppure no?
 
+function isPalindroma(parola) {
+    let parolaSenzaSpazi = parola.replace(/\s/g, "").toLowerCase();
+    let parolaInvertita = parolaSenzaSpazi.split("").reverse().join("");
+    return parolaSenzaSpazi === parolaInvertita;
+}
+
+let parolaUtente = prompt("Inserisci una parola:"); //Con l'ausilio di questa variabile, indico all'utente che dovrà inserire la parola che verrà esaminata.
+let risultato = isPalindroma(parolaUtente) ? "La parola è palindroma!" : "La parola non è palindroma."; //Questa variabile ha al suo interno una funziona che determinerà se la parole è palindroma o meno.
+
+console.log(risultato); //Con il console log l'utente vedrà stampato sulla console il risultato.
